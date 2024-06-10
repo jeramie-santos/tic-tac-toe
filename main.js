b663c4d6b20ee6a553411be6b39e2f28d6d0ff49
Jeramie Santos <jeramieramsantos79@gmail.com>
2024-06-10 20:33:43 +0800

    add gameboard

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ function createPlayer (name, mark) {
 
 const Jeramie = createPlayer("Jeramie", "X");
 const Max = createPlayer("Max", "O");
+
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    let board = [];
+
+    for (let i = 0; i< rows; i++){
+        board[i] = [];
+        for (let j = 0; j < columns; j++){
+            board[i].push("0");
+        }
+    }
+}
+
+Gameboard();
