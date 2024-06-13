0829039d57a95df5bb240ff49a868686e52b6052
Jeramie Santos <jeramieramsantos79@gmail.com>
2024-06-13 16:38:30 +0800

    displaying board, placing mark on console

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const h1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
+const table = document.querySelector("table");
+
 function Gameboard() {
     let board = [];
 
@@ -9,7 +13,26 @@ function Gameboard() {
     }
     
     const getBoard = () => board;
-    const logBoard = () => console.table(board);
+    const logBoard = () => {
+        table.innerHTML = `
+            <tr>
+                <td>${board[0][0]}</td>
+                <td>${board[0][1]}</td>
+                <td>${board[0][2]}</td>
+            </tr>
+            <tr>
+                <td>${board[1][0]}</td>
+                <td>${board[1][1]}</td>
+                <td>${board[1][2]}</td>
+            </tr>
+            <tr>
+                <td>${board[2][0]}</td>
+                <td>${board[2][1]}</td>
+                <td>${board[2][2]}</td>
+            </tr>
+        `;
+    };
+    
     const placeMark = function(row, column, mark) {
         board[row][column] = mark;
         logBoard();
@@ -22,18 +45,19 @@ function Gameboard() {
 function GameController() {
     const players = [
         {
-            playerName: "Jeramie",
+            name: "Jeramie",
             mark: "X",
         },
         {
-            playerName: "Max",
+            name: "Max",
             mark: "O",
         }
     ];
 
     let playerActive = players[0];
 
-    const printPlayer = () => `Player ${playerActive.playerName}'s turn`;
+    const printPlayer = () => `Player ${playerActive.name}'s turn`;
+    const printMark = () => `Mark ${playerActive.mark}`;
 
 
 
@@ -52,18 +76,20 @@ function GameController() {
         if (updateBoard[a][b] == "") {
             gameBoard.placeMark(a, b, playerActive.mark);
             switchPlayer();
-            console.log(printPlayer());
+            h1.textContent = printPlayer();
+            h2.textContent = printMark();
         } else {
             console.log("Cannot place a mark!");
         }
         
     }
 
-    return {playRound, printPlayer, logBoard};
+    return {playRound, printPlayer, printMark ,logBoard};
 }
 
 const game = GameController();
 
-console.log(game.printPlayer());
-console.log(game.logBoard());
+h1.textContent = game.printPlayer();
+h2.textContent = game.printMark();
+game.logBoard();
 
